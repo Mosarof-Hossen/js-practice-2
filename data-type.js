bdffe186f3d97c569c305a8029232f7130ee137b
Mosarof-Hossen <mmd58553@gmail.com>
2025-09-28 08:25:16 +0600

    js is a dynamic type proframming language.. typeof check value

---
+++ data-type.js
@@ -0,0 +1,24 @@
+//javascript is a dynamic type programming language
+
+let data =4;
+console.log(typeof data)
+let some=false;
+
+some={
+    name:"Mosarof",address:"diargarfa"
+}
+console.log(typeof some)
+// javascript একটা variable এর ভিতরে যে কোন মান বসানো  যায় পরিবর্তন করে অন্নও মান ও বসানো যায় 
+//js কি ধরনের ডাটা চেক করার জন্য typeof use kore 
+
+//একাধিক মান রাখা যায় object,array ,function
+
+
+//js:primitive data type
+const price=50;
+const name='bodrul';
+const isPoor=false;
+const isKnown=undefined;  //undefined // usually you should not set value to undefined
+let isLocal;    //undefined
+
+const merit=null; ///null value not set
